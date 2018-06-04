c2fe2bfdc1380874ac6c6024f0077c9aaf22235f
Timothy Meixell <misternu@gmail.com>
2018-06-04 11:13:14 -0500

    Test the double step

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -10,5 +10,9 @@ describe Enigma do
       enigma = Enigma.new(settings: ['M', 'C', 'K'])
       expect(enigma.run('QMJIDO MZWZJFJR')).to eq 'ENIGMA REVEALED'
     end
+    it 'does the double step' do
+      enigma = Enigma.new(settings: ['M', 'D', 'U'])
+      expect(enigma.run('WDUGU')).to eq 'HELLO'
+    end
   end
 end
